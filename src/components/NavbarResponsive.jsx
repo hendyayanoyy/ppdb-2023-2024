a3f6558c2cdc4a83699410edb08e2dfa46c5b06f
hendyayanoyy <hendyayanoyy05@gmail.com>
2024-02-05 13:46:58 +0700

    first

--- src/components/NavbarResponsive.jsx
+++ src/components/NavbarResponsive.jsx
@@ -26,24 +26,13 @@ function Navbar() {
 
   return (
     <div>
-      <nav>
-        <ul className={`flex text-white text-lg md:text-2xl gap-4 md:gap-8 p-4 fixed top-0 left-0 md:right-8 justify-end ${isScrolled ? 'bg-black bg-opacity-75' : ''}`}>
-          <ScrollLink activeClass="active" smooth spy to="home" className="hover:underline cursor-pointer">Beranda</ScrollLink>
-          <ScrollLink activeClass="active" smooth spy to="profile" className="hover:underline cursor-pointer">Profil</ScrollLink>
-          <ScrollLink activeClass="active" smooth spy to="information" className="hover:underline cursor-pointer">Informasi</ScrollLink>
-          <ScrollLink activeClass="active" smooth spy to="kontak" className="hover:underline cursor-pointer">Kontak</ScrollLink>
-        </ul>
-        <button
-          className={`fixed inset-0 z-40 ${isOpen ? 'block' : 'hidden'}`}
-          onClick={toggleNavbar}
-        >
-          {isOpen ? (
-            <img src={crossIcon} alt="Close" className="w-6 h-6" />
-          ) : (
-            <img src={hamburgerIcon} alt="Menu" className="w-6 h-6" />
-          )}
-        </button>
-      </nav>
+<nav className={`flex justify-end text-white text-lg md:text-2xl gap-4 md:gap-8 p-4 fixed top-0 left-0 md:right-8 ${isScrolled ? 'bg-black bg-opacity-75' : ''}`}>
+  <ScrollLink activeClass="active" smooth spy to="home" className="hover:underline cursor-pointer">Beranda</ScrollLink>
+  <ScrollLink activeClass="active" smooth spy to="profile" className="hover:underline cursor-pointer">Profil</ScrollLink>
+  <ScrollLink activeClass="active" smooth spy to="information" className="hover:underline cursor-pointer">Informasi</ScrollLink>
+  <ScrollLink activeClass="active" smooth spy to="kontak" className="hover:underline cursor-pointer">Kontak</ScrollLink>
+</nav>
+~
 
       {isOpen && (
         <div className="fixed inset-0 bg-black backdrop-blur-md opacity-50 z-30"></div>
